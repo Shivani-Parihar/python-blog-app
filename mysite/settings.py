10f87d1fc3e7855d823cee4dd48b179b789b0fa6
Shivani Singh <ssinghparihar@unomaha.edu>
2017-07-09 19:58:00 -0500

    Changes to settings file

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,15 +133,12 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
-
 STATICFILES_DIRS = (
-
 os.path.join(PROJECT_ROOT, 'static'),
-
 )
 
 # Update database configuration with $DATABASE_URL.
